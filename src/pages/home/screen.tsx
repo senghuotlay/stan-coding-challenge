de80b01ba65cbc14c892c3c41f46789a69933910
dalley8626 <senghuotlay86@gmail.com>
2024-03-22 00:17:20 +1100

    add open sans as default font

--- src/pages/home/screen.tsx
+++ src/pages/home/screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Home = () => {
-  return <h2>Home</h2>;
+  return <h1>Home</h1>;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
