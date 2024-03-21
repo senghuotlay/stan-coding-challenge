b5af6e54ca7fcb8012e036d7a3b9a6ca43fc9d9a
dalley8626 <senghuotlay86@gmail.com>
2024-03-21 22:38:30 +1100

    init webpack project

---
+++ index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const domNode = document.getElementById("root")!;
+const root = ReactDOM.createRoot(domNode);
+
+root.render(<App />);
