05a737a563bc25194efa31b1614557d7f30f22ec
dalley8626 <senghuotlay86@gmail.com>
2024-03-22 16:45:51 +1100

    add query hook and display data on home screen

---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+type Props = {
+  apiUrl: string;
+  skip?: boolean;
+};
+
+export const useQuery = <T>({ apiUrl, skip }: Props) => {
+  const [data, setData] = useState<T | undefined>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (skip) {
+        return;
+      }
+      setLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      try {
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+        setData(result);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        } else {
+          setError('An unknown error occurred. please try again later.');
+        }
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
--- src/pages/home/screen.tsx
+++ src/pages/home/screen.tsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import { useQuery } from '../../hooks/useQuery';
+import { Carousel } from '../../components/Carousel/Carousel';
+import {
+  CarouselCard,
+  CarouselCardSkeleton,
+} from '../../components/CarouselCard';
+import { useNavigate } from 'react-router-dom';
+import { Program } from '../../types/Program';
 
 export const Home = () => {
-  return <h2>Home</h2>;
+  const { data, loading, error } = useQuery<Program[]>({
+    apiUrl: '/data.json',
+  });
+
+  const navigate = useNavigate();
+
+  const navigateToProgram = (item: Program) => {
+    navigate(`/program/${item.id}`, { state: item });
+  };
+
+  return (
+    <Carousel
+      data={data || []}
+      isLoading={loading}
+      LoadingComponent={<CarouselCardSkeleton />}
+      renderItem={(item) => (
+        <CarouselCard
+          key={item.id}
+          title={item.title}
+          imageUrl={item.image}
+          onClick={() => navigateToProgram(item)}
+        />
+      )}
+      onFocusedPress={navigateToProgram}
+      ErrorComponent={
+        <div>
+          <p
+            style={{
+              color: '#606060',
+              fontSize: '1.5rem',
+              fontWeight: 'bold',
+            }}
+          >
+            {error || 'An unkonwn error occured. Please try again later.'}
+          </p>
+        </div>
+      }
+      isError={!!error}
+    />
+  );
 };
